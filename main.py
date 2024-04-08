fb3a36af08948c2126df0193f3ed67b25dbe772b
Maksim Belosokhov <maks3010dan@gmail.com>
2024-04-08 16:55:56 +0300

    add new code in main and home.html

--- main.py
+++ main.py
@@ -7,6 +7,19 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "iufhfijghiyweh"
 socketio = SocketIO(app)
 
+rooms = {}
+
+def generate_unique_code(length):
+    while True:
+        code = ""
+        for _ in range (length):
+            code += random.choice(ascii_uppercase)
+
+        if code not in rooms:
+            break
+
+    return code
+
 @app.route("/", methods=["POST", "GET"])
 def home():
     if request.method == "POST":
@@ -14,6 +27,19 @@ def home():
         code = request.form.get("code")
         join = request.form.get("join", False)
         create = request.form.get("create", False)
+
+        if not name:
+            return render_template("home.html", error="Please enter a name.")
+
+        if join != False and not code:
+            return render_template("home.html", error="Please enter a room code.")
+
+        room = code
+        if create != False:
+            room = generate_unique_code(4)
+            rooms[room] = {"members": 0, "messages": []}
+        elif code not in rooms:
+            return render_template("home.html", error="Room does not exist.")
 
     return render_template("home.html")
 
